015e8dd6346e00b4df98a938da758cc5f8f32963
Joseph Wortmann <joseph.wortmann@gmail.com>
2023-04-02 23:03:41 +0000

    read version from setup.cfg

--- publish_layers.py
+++ publish_layers.py
@@ -1,3 +1,4 @@
+from configparser import ConfigParser
 from sys import argv
 
 import boto3
@@ -6,8 +7,10 @@ echostream_node_zip = None
 with open(argv[1], "rb") as f:
     echostream_node_zip = f.read()
 
-layer_name = f'echostream-node-{argv[2].replace(".", "_")}'
-echocore_arns = dict()
+config = ConfigParser()
+config.read("setup.cfg")
+
+layer_name = f'echostream-node-{config["metadata"]["version"].replace(".", "_")}'
 for region_name in ("us-east-1", "us-east-2", "us-west-1", "us-west-2"):
     print(f"Publishing {layer_name} to {region_name}")
     lambda_client = boto3.client("lambda", region_name=region_name)
